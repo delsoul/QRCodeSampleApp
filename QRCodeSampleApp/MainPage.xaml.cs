36f186acef40dc025fde7ece9898b9edcc5519e3
Dmitry Solodov <dmitry.creator@gmail.com>
2021-07-06 21:37:47 +0300

    Добавьте файлы проекта.

---
+++ QRCodeSampleApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QRCodeSampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
