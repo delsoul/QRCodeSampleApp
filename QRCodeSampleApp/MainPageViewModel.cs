a76da742fdfc89571e1d7ccc79c8d6f96ce40318
Dmitry Solodov <dmitry.creator@gmail.com>
2021-07-08 08:10:06 +0300

    press F to pay respect

--- QRCodeSampleApp/MainPageViewModel.cs
+++ QRCodeSampleApp/MainPageViewModel.cs
@@ -2,27 +2,21 @@
 using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using NickBuhro.Translit;
 using Xamarin.Forms;
 
 namespace QRCodeSampleApp
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
-        public string FullName { get; set; }
-        public string Organization { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Adress { get; set; }
-
-        public bool BarcodeIsVisible { get; set; } = false;
-
+        public string FullName { get; set; } = "Гаязов Рифат Равилович";
+        public string PhoneNumber { get; set; } = "+78005553535";
+        public string Email { get; set; } = "gayazovrr@kamaz.ru";
         public string BarcodeValue { get; set; }
-
         public ICommand GenerateQRCommand { get; set; }
 
         public MainPageViewModel()
         {
+
             GenerateQRCommand = new Command(async () =>  await GenerateQR());
         }
 
@@ -32,10 +26,7 @@ namespace QRCodeSampleApp
             {
                 Translitor translitor = new Translitor();
                 string fn = translitor.Translit(FullName);
-                string org = translitor.Translit(Organization);
-                string adr = translitor.Translit(Adress);
-                BarcodeValue = $"MECARD:N:{fn};ORG:{org};TEL:{PhoneNumber};EMAIL:{Email};ADR:{adr};;";
-                BarcodeIsVisible = true;
+                BarcodeValue = $"MECARD:N:{fn};ORG:;TEL:{PhoneNumber};EMAIL:{Email};ADR:;;";
             }
             catch (Exception ex)
             {
